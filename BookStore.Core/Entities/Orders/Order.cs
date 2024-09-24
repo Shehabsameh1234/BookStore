824ff5330da06641db06ccacbf8eece7a47825bf
shehab sameh <shehabsameh987123@gmail.com>
2024-09-24 14:02:58 +0300

    update

--- BookStore.Core/Entities/Orders/Order.cs
+++ BookStore.Core/Entities/Orders/Order.cs
@@ -5,5 +5,12 @@ namespace BookStore.Core.Entities.Orders
     public class Order:BaseEntity
     {
         public string BuyerEmail { get; set; }
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.UtcNow;
+        public OrderStatus OrderStatus { get; set; } = OrderStatus.Pending;
+        public OrderAddress OrderAddress { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; }
+        
+
+
     }
 }
