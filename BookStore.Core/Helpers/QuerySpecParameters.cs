f2b77a3c7f4495a65f3a774a8fa2fd2866695a63
shehabsameh1234 <shehabsameh987123@gmail.com>
2024-10-14 21:08:46 +0300

    update

--- BookStore.Core/Helpers/QuerySpecParameters.cs
+++ BookStore.Core/Helpers/QuerySpecParameters.cs
@@ -8,12 +8,12 @@ namespace BookStore.Core.Helpers
 {
     public class QuerySpecParameters
     {
-        private const int MaxPageSize = 10;
+       
         private int pageSize=5;
         public int PageSize
         {
             get { return pageSize; }
-            set { pageSize = value >MaxPageSize ? MaxPageSize :value; }
+            set { pageSize = value; }
         }
         public int PageIndex { get; set; } = 1;
         public string? Sort { get; set; }
