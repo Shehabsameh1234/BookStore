3d98ecb64a3380e32d76c60e2a09e67abf882f32
shehabsameh1234 <shehabsameh987123@gmail.com>
2024-11-05 22:35:31 +0200

    update

--- BookStore.Core/Dtos/RegisterDto.cs
+++ BookStore.Core/Dtos/RegisterDto.cs
@@ -17,7 +17,7 @@ namespace BookStore.Core.Dtos
         [Required]
         public string PhoneNumber { get; set; }
         [Required]
-        [RegularExpression("^(?=.*[A-Z])(?=.*\\d)(?=.*[a-zA-Z]).{6,}$",
+        [RegularExpression("^(?=.*[A-Z])(?=.*\\d)(?=.*[a-zA-Z0-9]).{8,}$",
         ErrorMessage = "password must contain at least one number , one upper case charachter ,one alphanumeric")]
         public string Password { get; set; }
     }
